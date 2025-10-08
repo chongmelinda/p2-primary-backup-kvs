bf6048004258705eb44e8bede821f149c069c8fe
Mel C <chongmel@umich.edu>
2025-10-08 01:12:35 -0400

    6/9 tests

--- viewservice/server_impl.go
+++ viewservice/server_impl.go
@@ -74,58 +74,73 @@ func (vs *ViewServer) GetImpl(args *GetArgs, reply *GetReply) error {
 func (vs *ViewServer) tick() {
 	// TODO 1: For each known server, check if (current tick - lastHeard[server]) > DeadPings.
     //         If yes, mark the server as dead.
-	for server, lastTick := range vs.impl.lastPing {
-		if vs.impl.tickCount - lastTick > DeadPings {
-			//if we're in here, it's dead
-			if vs.impl.currentView.Primary == server && vs.impl.serverView[vs.impl.currentView.Primary] == vs.impl.currentView.Viewnum {
+	    vs.impl.tickCount++
+		
+		// primary := vs.impl.currentView.Primary
+		// backup := vs.impl.currentView.Backup
+		for server, lastTick := range vs.impl.lastPing {
+			changed_view := false
+			if vs.impl.tickCount - lastTick > DeadPings {
+				//if we're in here, it's dead
 				// TODO 2: If the primary is dead:
 				//           - Only promote the backup to primary *if* the old primary had ACKed its view. (and do other 3 items)
 				//           - Increment the view number.
 				//           - Set backup = "" (will be filled later by idle servers).
-				vs.impl.currentView.Viewnum++
-				vs.impl.currentView.Backup = ""
-			}
-
-			if vs.impl.currentView.Backup == server {
-				// TODO 3: If the backup is dead:
-				//           - Remove it.
-				//           - Increment the view number.
-				vs.impl.currentView.Backup = ""
-				vs.impl.currentView.Viewnum++
-			}
-
-			if vs.impl.currentView.Backup == "" {
-				// TODO 4: If there is no backup:
-				//           - Pick one of the idle servers (not primary, not already backup).
-				//           - Assign it as backup.
-				//           - Increment the view number.
-				for server := range vs.impl.lastPing {
-					// no dead servers
-					if vs.impl.tickCount - vs.impl.lastPing[server] > DeadPings {
-						continue
+				if vs.impl.currentView.Primary != "" && (vs.impl.tickCount - vs.impl.lastPing[vs.impl.currentView.Primary] > DeadPings || vs.impl.serverView[vs.impl.currentView.Primary] == 0) {
+					// primary is dead or restarted
+					if vs.impl.currentView.Backup != "" {
+						vs.impl.currentView.Primary = vs.impl.currentView.Backup
+						vs.impl.currentView.Backup = ""
+					} else {
+						vs.impl.currentView.Primary = ""
 					}
+					changed_view = true
+				}
 
-					// no primary/backup
-					if server == vs.impl.currentView.Primary || server == vs.impl.currentView.Backup {
-						continue
+				if vs.impl.currentView.Backup == server {
+					// TODO 3: If the backup is dead:
+					//           - Remove it.
+					//           - Increment the view number.
+					vs.impl.currentView.Backup = ""
+					changed_view = true
+				}
+
+				if vs.impl.currentView.Backup == "" {
+					// TODO 4: If there is no backup:
+					//           - Pick one of the idle servers (not primary, not already backup).
+					//           - Assign it as backup.
+					//           - Increment the view number.
+					for server := range vs.impl.lastPing {
+						// no dead servers
+						if vs.impl.tickCount - vs.impl.lastPing[server] > DeadPings {
+							continue
+						}
+
+						// no primary/backup
+						if server == vs.impl.currentView.Primary || server == vs.impl.currentView.Backup {
+							continue
+						}
+
+						//idle server found
+						vs.impl.currentView.Backup = server
+						changed_view = true
+						break
 					}
+				}
 
-					//idle server found
-					vs.impl.currentView.Backup = server
+				if vs.impl.serverView[server] == 0 {
+					// TODO 5: If a server restarted (detected because it Pings with viewnum=0):
+					//           - It cannot continue as primary/backup.
+					//           - Remove it from those roles.
+					//           - Treat it as an idle server (eligible for backup in future views).
+					vs.impl.currentView.Primary = ""
+					vs.impl.currentView.Backup = ""
+				}
+
+				if changed_view {
 					vs.impl.currentView.Viewnum++
-					break
 				}
 			}
-
-			if vs.impl.serverView[server] == 0 {
-				// TODO 5: If a server restarted (detected because it Pings with viewnum=0):
-				//           - It cannot continue as primary/backup.
-				//           - Remove it from those roles.
-				//           - Treat it as an idle server (eligible for backup in future views).
-				vs.impl.currentView.Primary = ""
-				vs.impl.currentView.Backup = ""
-			}
-		}
 	}
 
     // TODO 6: Make sure you don’t promote backup → primary 
